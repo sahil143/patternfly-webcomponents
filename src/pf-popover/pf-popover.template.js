a6876575a997f37e2e1258a7ba0924edfd1ae072
sahil143 <sahilbudhwar143@gmail.com>
2017-07-06 18:41:17 +0530

    pf-popover

---
+++ src/pf-popover/pf-popover.template.js
@@ -0,0 +1,15 @@
+const PfPopoverTemplate = `
+<div class="popover" role="popover">
+  <div class="arrow"></div>
+  <h3 class="popover-title closable"></h3>
+  <div class="popover-content"></div>
+</div>
+`;
+
+const pfPopoverDismissibleButton = `
+<button type="button" class="close">
+<span class="pficon pficon-close"></span>
+</button>`;
+
+export { PfPopoverTemplate as default };
+export { pfPopoverDismissibleButton as tmp };
