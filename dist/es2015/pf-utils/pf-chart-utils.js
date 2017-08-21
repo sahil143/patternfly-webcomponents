672c1dc70d93ddf5c3963dfb208778957cbb7ffc
sahil143 <sahil143@redhat.com>
2017-08-21 16:16:47 +0000

    chore(release): updates generated by Travis build

---
+++ dist/es2015/pf-utils/pf-chart-utils.js
@@ -0,0 +1,63 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * --------------------------------------------------------------------------
+ * PfChartUtil
+ * Internal Utility Functions for Patternfly Web Components
+ * --------------------------------------------------------------------------
+ */
+
+var PfChartUtil = function () {
+  function PfChartUtil() {
+    _classCallCheck(this, PfChartUtil);
+  }
+  /**
+   * generate chart
+   * @param {object} config
+   */
+
+
+  _createClass(PfChartUtil, [{
+    key: "generate",
+    value: function generate(config) {
+      return c3.generate(config);
+    }
+
+    /**
+     * c3 load function
+     * @param {} chart
+     * @param {object} obj
+     */
+
+  }, {
+    key: "load",
+    value: function load(chart, obj) {
+      chart.load(obj);
+    }
+
+    /**
+     * c3 unload function
+     * @param {} chart
+     * @param {object} obj
+     */
+
+  }, {
+    key: "unload",
+    value: function unload(chart, obj) {
+      chart.unload(obj);
+    }
+  }]);
+
+  return PfChartUtil;
+}();
+
+var pfChartUtil = new PfChartUtil();
+exports.pfChartUtil = pfChartUtil;
